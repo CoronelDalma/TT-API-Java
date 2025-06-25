1c7e0bb568d166eedc5505657187f8cf974985a7
CoronelDalma <coronel.dg@gmail.com>
2025-06-25 15:32:46 -0300

    db configuration

---
+++ src/test/java/com/talento/ApiEcommerce/ApiEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talento.ApiEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
